3d02ad88334ffca99cd18c554c1d74ed0439329d
jon <jonstarks6@gmail.com>
2025-03-16 15:43:30 -0400

    Add initial store app structure with home view and templates

--- redpart/store_app/views.py
+++ redpart/store_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Shoe
 
 # Create your views here.
+
+def home(request):
+    shoes = Shoe.objects.all()
+    return render(request, 'home.html', {'shoes': shoes})
