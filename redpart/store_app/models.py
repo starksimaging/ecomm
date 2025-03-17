dd3a04443518a04289ced1fa6339e2cfb4be6cd3
jon <jonstarks6@gmail.com>
2025-03-17 07:39:08 -0400

    Update shoe price field to allow higher values and optimize image rendering in home template

--- redpart/store_app/models.py
+++ redpart/store_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Shoe(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=3, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
     image = models.ImageField(upload_to='shoes')
 
